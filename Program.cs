3d80053523a75fa631ab9daaf359c23b676bfae8
dikoleshubh <dikole.shubh@gmail.com>
2021-04-21 03:27:46 +0530

    EMPLOYEE wage calculation & management for multiple Companies

--- Program.cs
+++ Program.cs
@@ -9,7 +9,7 @@ namespace Employee_Wage
             Console.WriteLine("EMPLOYEE WAGE ");
             EmpWageBuilderArray empWageBuilder = new EmpWageBuilderArray();
             empWageBuilder.addComapnyEmpWage("Dmart", 20, 2, 10);  //call method
-            empWageBuilder.addComapnyEmpWage("Relicance", 10, 4, 20); //call method
+            empWageBuilder.addComapnyEmpWage("WhateverMart", 10, 4, 20); //call method
             empWageBuilder.Wage_Compute(); //call method
 
 
