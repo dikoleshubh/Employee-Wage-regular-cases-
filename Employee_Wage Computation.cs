c4efd258ffad292a1f7b71e366aeba2775255b3f
dikoleshubh <dikole.shubh@gmail.com>
2021-04-21 02:42:32 +0530

    EMPLOYEE wage calculation for diffrent company & storing result

--- Employee_Wage Computation.cs
+++ Employee_Wage Computation.cs
@@ -6,9 +6,28 @@ using System.Threading.Tasks;
 
 namespace Employee_Wage
 {
-    class WageComputation
+    class EmployeeWageComputation
     {
-        public static int Wage_Compute(string company, int EMP_RATE_PER_HOUR,int workingDays,int maxhours)
+        
+
+        public string company;
+        public int empRatePerHour;
+        public int numOfWorkingDays;
+        public int maxHoursPerMonth;
+        public int totalempwage;
+        public EmployeeWageComputation(string company, int empRatePerHour, int WorkingDays, int maxhours)
+        {
+            this.company = company;
+            this.empRatePerHour = empRatePerHour;
+            this.numOfWorkingDays = WorkingDays;
+            this.maxHoursPerMonth = maxhours;
+
+        }
+    
+        
+    
+
+        public  void Wage_Compute()
         {
             const int FULL_TIME = 1; 
             const int PART_TIME = 2;
@@ -19,7 +38,7 @@ namespace Employee_Wage
             Random random = new Random();
             int EmpCheck = random.Next(0, 3); ////Random Function for Employee selection
             
-            while (days < workingDays && emphrs < maxhours)////condtion check for Employee exceeding days
+            while (days < this.numOfWorkingDays && emphrs < this.maxHoursPerMonth)////condtion check for Employee exceeding days
             {
                 days++;
 
@@ -36,12 +55,20 @@ namespace Employee_Wage
                         break;
                 }
             }
-            empWage = EMP_RATE_PER_HOUR * emphrs;//// Emp Wage calculation/day
+            empWage = empRatePerHour * emphrs;//// Emp Wage calculation/day
             totalempwage = empWage * days;
+                                                      //Display empwage
+            totalempwage = totalempwage + empWage;      //Calculate total employe month wage
+            empWage = empRatePerHour * emphrs;
+
             Console.WriteLine($"Name of Compnay is {company} " );
             Console.WriteLine("Employe Wage Per Day & Month:- " + empWage +totalempwage);
-            return (totalempwage);
             
+            
+        }
+        public string toString()
+        {
+            return "Total Emp Wage for company :- " + company + "is :- " + totalempwage;
         }
     }
 }
